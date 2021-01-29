9881e87520a0cccc6a6cfe28e7d1f26b6a02fa52
Benoit Jeaurond <benoit@jeaurond.dev>
2021-01-29 18:06:14 -0500

    Added way to disable WDYR

--- wdyr.ts
+++ wdyr.ts
@@ -1,8 +1,9 @@
 /// <reference types="@welldone-software/why-did-you-render" />
 
 import React from 'react';
+import { WDYR } from '@env';
 
-if (process.env.NODE_ENV === 'development') {
+if (process.env.NODE_ENV === 'development' && WDYR === 'true') {
   const whyDidYouRender = require('@welldone-software/why-did-you-render');
   whyDidYouRender(React, {
     trackAllPureComponents: true,
