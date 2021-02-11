bdfc4fd0aef8e78e39bee54f01db76ba9a2334f6
Benoit Jeaurond <benoit@jeaurond.dev>
2021-02-10 20:05:00 -0500

    Fixed splash screen

--- android/app/src/main/java/com/cluenotes/MainActivity.java
+++ android/app/src/main/java/com/cluenotes/MainActivity.java
@@ -1,6 +1,13 @@
 package com.cluenotes;
 
+import android.os.Bundle;
+import androidx.annotation.Nullable;
 import com.reactnativenavigation.NavigationActivity;
 
 public class MainActivity extends NavigationActivity {
+  @Override
+  protected void onCreate(@Nullable Bundle savedInstanceState) {
+      super.onCreate(savedInstanceState);
+      setContentView(R.layout.background_splash);
+  }
 }
